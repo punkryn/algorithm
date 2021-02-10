30de5dd8dc9696dcfd482abd5cc41c7806ef0256
punkryn <gns8167@naver.com>
2021-02-10 14:13:31 +0900

    2021-02-10 realization (rewrite problem 3)

--- ActualProblem/Realization/stringCompression.py
+++ ActualProblem/Realization/stringCompression.py
@@ -1,55 +1,85 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60057
 # ababcdcdababcdcd
 
+# def solution(s):
+#     answer = 0
+#
+#     length = len(s)
+#
+#     count = 1
+#     result = []
+#     sw = 0
+#     for i in range(1, length // 2 + 1):
+#         tmp = ''
+#         pre = 0
+#         # for j in range(0, length - i):
+#         j = 0
+#         count = 1
+#         sw = 0
+#         while j < length - i:
+#             # print(s[j:j + i], s[j + i:j + i + i])
+#             #print(j, i)
+#             #print(s[j:j + i], s[j + i:j + i + i])
+#             if s[j:j + i] == s[j + i:j + i + i]:
+#                 pre = count
+#                 count += 1
+#                 j += i
+#                 sw = 1
+#             else:
+#                 if sw == 1:
+#                     tmp += ((str)(count if count != 1 else '') + s[j: j + i])
+#                 elif sw == 2:
+#                     tmp += ((str)(count if count != 1 else '') + s[j])
+#                 # print(tmp)
+#
+#                 count = 1
+#                 if sw == 1:
+#                     j += i
+#                 elif sw == 2:
+#                     j += 1
+#                 sw = 2
+#         if sw == 1:
+#             tmp += ((str)(count if count != 1 else '') + s[j: j + i])
+#         elif sw == 2:
+#             tmp += s[j:]
+#         print(tmp)
+#         result.append(len(tmp)) if  len(tmp) > 0 else 1
+#     print(result)
+#     answer = min(result)
+#     return answer
+
 def solution(s):
     answer = 0
 
     length = len(s)
 
-    count = 1
     result = []
-    sw = 0
+
+    if length <= 1:
+        return length
+
     for i in range(1, length // 2 + 1):
         tmp = ''
-        pre = 0
-        # for j in range(0, length - i):
-        j = 0
         count = 1
         sw = 0
-        while j < length - i:
+        for j in range(0, length - i, i):
             # print(s[j:j + i], s[j + i:j + i + i])
             #print(j, i)
             #print(s[j:j + i], s[j + i:j + i + i])
             if s[j:j + i] == s[j + i:j + i + i]:
-                pre = count
                 count += 1
-                j += i
-                sw = 1
             else:
-                if sw == 1:
-                    tmp += ((str)(count if count != 1 else '') + s[j: j + i])
-                elif sw == 2:
-                    tmp += ((str)(count if count != 1 else '') + s[j])
+                tmp += ((str)(count if count != 1 else '') + s[j: j + i])
+                count = 1
                 # print(tmp)
 
-                count = 1
-                if sw == 1:
-                    j += i
-                elif sw == 2:
-                    j += 1
-                sw = 2
-        if sw == 1:
-            tmp += ((str)(count if count != 1 else '') + s[j: j + i])
-        elif sw == 2:
-            tmp += s[j:]
+        tmp += str(count if count != 1 else '') + s[j + i:]
         print(tmp)
         result.append(len(tmp)) if  len(tmp) > 0 else 1
     print(result)
     answer = min(result)
     return answer
 
-s = '123'
-print(s[2:3])
 
 s = input()
 

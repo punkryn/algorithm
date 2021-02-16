5cc3e349bb452b32b94e60f3cacebb7872b1dc66
punkryn <gns8167@naver.com>
2021-02-16 19:34:50 +0900

    2021-02-16 dfsbfs

--- ActualProblem/DFSBFS/moveBlock.py
+++ ActualProblem/DFSBFS/moveBlock.py
@@ -1,191 +1,58 @@
-import sys
-sys.setrecursionlimit(5000)
+from collections import deque
+
+def check(pos, board):
+    next_pos = []
+    pos = list(pos)
+    x1, y1, x2, y2 = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    for i in range(4):
+        nx1, ny1, nx2, ny2 = x1 + dx[i], y1 + dy[i], x2 + dx[i], y2 + dy[i]
+
+        if board[nx1][ny1] == 0 and board[nx2][ny2] == 0:
+            next_pos.append({(nx1, ny1), (nx2, ny2)})
+
+    if x1 == x2:
+        for i in [-1, 1]:
+            if board[x1 + i][y1] == 0 and board[x2 + i][y2] == 0:
+                next_pos.append({(x1 + i, y1), (x1, y1)})
+                next_pos.append({(x2 + i, y2), (x2, y2)})
+
+    elif y1 == y2:
+        for i in [-1, 1]:
+            if board[x1][y1 + i] == 0 and board[x2][y2 + i] == 0:
+                next_pos.append({(x1, y1 + i), (x1, y1)})
+                next_pos.append({(x2, y2 + i), (x2, y2)})
+
+    return next_pos
+
 
 def solution(board):
     answer = 0
+    n = len(board)
+    newBoard = [[1] * (n + 2) for _ in range(n + 2)]
+    for i in range(n):
+        for j in range(n):
+            newBoard[i+1][j+1] = board[i][j]
+    q = deque()
+    visited = []
 
-    length = len(board)
-    global min_value
-    visited = [[0] * length for _ in range(length)]
-    rotate_visited = [[False] * length for _ in range(length)]
-    dfs(((0, 0), (0, 1)), visited, length, board, 1, rotate_visited)
+    pos = {(1, 1), (1, 2)}
+    q.append((pos, 0))
 
-    answer = visited[length-1][length-1]
+    visited.append(pos)
 
-    answer = min_value
+    while q:
+        pos, cost = q.popleft()
 
-    return answer - 1
+        if (n, n) in pos:
+            return cost
 
-def dfs(start, visited, length, board, counter, rotate_visited):
-    p1, p2 = start
+        for next_pos in check(pos, newBoard):
+            if next_pos not in visited:
+                q.append((next_pos, cost + 1))
+                visited.append(next_pos)
 
-    x1, y1 = p1
-    x2, y2 = p2
-
-    global min_value
-
-    visited[x1][y1] = counter
-    visited[x2][y2] = counter
-
-    if (x1, y1) == (length - 1, length - 1) or (x2, y2) == (length - 1, length - 1):
-        #print('11111111111111111111111111111111111111111111111')
-        #visited[length-1][length-1] = min(visited[x1][y1], counter)
-        if min_value > counter:
-            min_value = counter
-        return
-
-
-
-    # for i in range(length):
-    #     for j in range(length):
-    #         print(visited[i][j], end=' ')
-    #     print()
-    # print()
-
-    for i in range(4):
-        nx1 = x1 + dx[i]
-        ny1 = y1 + dy[i]
-
-        nx2 = x2 + dx[i]
-        ny2 = y2 + dy[i]
-
-        if not (0 <= nx1 < length and 0 <= ny1 < length):
-            continue
-        if not (0 <= nx2 < length and 0 <= ny2 < length):
-            continue
-
-        if visited[nx1][ny1] and visited[nx2][ny2]:
-            continue
-
-        if board[nx1][ny1] == 1 or board[nx2][ny2] == 1:
-            continue
-
-        #print(nx1, ny1, nx2, ny2)
-
-        pre1 = visited[nx1][ny1]
-        pre2 = visited[nx2][ny2]
-
-        dfs(((nx1, ny1), (nx2, ny2)), visited, length, board, counter + 1, rotate_visited)
-
-        visited[nx1][ny1] = pre1
-        visited[nx2][ny2] = pre2
-
-    for i in range(4, 6):
-        index = i - 4
-
-        axis = (x1, y1)
-        pos = (x2, y2)
-
-        # if index == 0:
-        #     ob = (x2 + y2 - 1, abs(x2 - y2 - 1))
-        # elif index == 1:
-        #     ob = (abs(y2 - x2 - 1), y2 + x2 - 1)
-
-        nx, ny, ob = rotate(axis, pos, index)
-
-        if not (0 <= nx < length and 0 <= ny < length):
-            continue
-
-        if not (0 <= ob[0] < length and 0 <= ob[1] < length):
-            continue
-
-
-        if visited[nx][ny]:
-            continue
-
-        if board[ob[0]][ob[1]] == 1:
-            continue
-
-        if visited[nx][ny] + 1 == visited[pos[0]][pos[1]]:
-            continue
-
-        if board[nx][ny] == 1:
-            continue
-
-        pre = visited[nx][ny]
-
-        #print('1', axis, pos, nx, ny, ob, index)
-        dfs((axis, (nx, ny)), visited, length, board, counter + 1, rotate_visited)
-
-        visited[nx][ny] = pre
-
-        ########################################################
-
-    for i in range(4, 6):
-        index = i - 4
-        axis = (x2, y2)
-        pos = (x1, y1)
-
-        # if index == 0:
-        #     ob = (x1 + y1 - 1, abs(x1 - y1 - 1))
-        # elif index == 1:
-        #     ob = (abs(y1 - x1 - 1), x1 + y1 - 1)
-
-        nx, ny, ob = rotate(axis, pos, index)
-        if not (0 <= nx < length and 0 <= ny < length):
-            continue
-
-        if not (0 <= ob[0] < length and 0 <= ob[1] < length):
-            continue
-
-
-        if visited[nx][ny]:
-            continue
-
-        if board[ob[0]][ob[1]] == 1:
-            continue
-
-        if visited[nx][ny] + 1 == visited[pos[0]][pos[1]]:
-            continue
-
-        if board[nx][ny] == 1:
-            continue
-
-        pre = visited[nx][ny]
-
-        #print('2', nx, ny, ob)
-        dfs((axis, (nx, ny)), visited, length, board, counter + 1, rotate_visited)
-
-        visited[nx][ny] = pre
-
-def rotate(axis, pos, d):
-    x, y = pos
-    x_, y_ = axis
-    clockwise = 0
-    counterclockwise = 1
-
-    if x == x_ and y > y_:
-        if d == clockwise:
-            return x_ + 1, y_, (x + 1, y)
-        else:
-            return x_ - 1, y_, (x - 1, y)
-    elif x == x_ and y < y_:
-        if d == clockwise:
-            return x_ - 1, y_, (x - 1, y)
-        else:
-            return x_ + 1, y_, (x + 1, y)
-    elif x < x_ and y == y_:
-        if d == clockwise:
-            return x_, y_ + 1, (x, y + 1)
-        else:
-            return x_, y_ - 1, (x, y - 1)
-    else:
-        if d == clockwise:
-            return x_, y_ - 1, (x, y - 1)
-        else:
-            return x_, y_ + 1, (x, y + 1)
-
-
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-clock = [0, 1]
-
-min_value = 1e9
-
-#board = [[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]
-board = [[0,0,0,0,1],[1,1,0,0,1],[1,1,0,1,1],[1,1,0,0,1],[0,0,0,0,0]]
-print(solution(board))
+    return 0

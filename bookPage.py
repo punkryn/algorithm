a384ebc29639253acebc1d3bb3275eb3adb51d17
punkryn <gns8167@naver.com>
2021-03-01 21:55:29 +0900

    2021-03-01 bookpage

--- bookPage.py
+++ bookPage.py
@@ -2,61 +2,79 @@
 
 # https://www.acmicpc.net/problem/1019
 
-import math
+from sys import stdin
 
-def digit_length(n):
-    return int(math.log10(n)) + 1 if n else 0
+def calc(n, point):
+    while n > 0:
+        ans[n % 10] += point
+        n = n // 10
 
-n = int(input())
+n = int(stdin.readline())
 
-#dp = [0] * (n + 1)
+ans = [0] * 10
 
-num = [0] * 10
-
-# count = 0
-# for i in range(1, 11):
-#     for j in range(1, 11):
-#         if '1' in str(count):
-#             print(count, end='  ')
-#         else:
-#             print(count, end=' ')
-#         count += 1
-#     print()
-
-length = digit_length(n)
-
-biggest = n // (10 ** (length - 1))
-
-a = 0
-zero = 0
-for i in range(3, length + 1):
-    under = (9 * (10 ** (i - 2))) * (i - 1)
-    zero = int(9 * (10 ** (i - 3))) * (i - 2)
-    a = int((under - zero) / 9)
-    num[0] += zero
-
-    for j in range(1, 10):
-        num[j] += a
-print(num)
-
-
-# for i in range(1, biggest):
-#     num[i] += 1
+# def solution(a, b, count):
+#     #print('a, b', a, b)
+#     if a > b:
+#         return
+#     else:
+#         while b % 10 != 9 and a <= b:
+#             calc(b, count)
+#             b -= 1
 #
-#     num[0] += zero
-#     for j in range(1, 10):
-#         num[j] += a
+#         while a % 10 != 0 and a <= b:
+#             calc(a, count)
+#             a += 1
 #
-# print(num)
+#         if a == b:
+#             return
+#
+#         for i in range(10):
+#             ans[i] += ((b // 10) - (a // 10) + 1) * (10 ** (count - 1))
+#
+#         solution(a // 10, b // 10, count + 1)
+#
+# # tmp = 0
+# # ctmp = 1
+# # zero = 0
+# # for _ in range(len(str(n)) - 1):
+# #     ans[0] += tmp
+# #     for i in range(1, 10):
+# #         ans[i] += (ctmp + tmp * 9)
+# #
+# #     tmp = (ctmp + tmp * 9)
+# #     ctmp *= 10
+#
+# #solution(10 ** (len(str(n)) - 1), n, 1)
+# solution(1, n, 1)
+#
+# for i in range(10):
+#     print(ans[i], end=' ')
 
-def roof(num, index):
-    l = int(str(num)[index])
-    kkk = length - index
-    for i in range(1, l):
-        under = (9 * (10 ** (kkk - 2))) * (kkk - 1)
-        zero = int(9 * (10 ** (kkk - 3))) * (kkk - 2)
-        a = int((under - zero) / 9)
-        num[0] += zero
-        num[i] += (10 ** (kkk - 1))
-        for j in range(1, 10):
-            num[j] += (a)
+point = 1
+start = 1
+
+while(start <= n):
+    #print(start, n)
+    while(n % 10 != 9 and start <= n):
+        calc(n, point)
+        n -= 1
+    #print('n',n)
+    if n < start:
+        break
+
+    while start % 10 != 0 and start <= n:
+        calc(start, point)
+        start += 1
+    #print('start', start)
+    start //= 10
+    n //= 10
+    #print(n, start)
+    #print((n - start + 1) * point)
+    for i in range(10):
+        ans[i] += ((n - start + 1) * point)
+
+    point *= 10
+
+for i in range(10):
+    print(ans[i], end=' ')

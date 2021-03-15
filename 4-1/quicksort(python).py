e768328439c408dee134a488fc3d15ed1c0c209e
punkryn <gns8167@naver.com>
2021-03-15 15:56:13 +0900

    2021-03-15 quicksort

---
+++ 4-1/quicksort(python).py
@@ -0,0 +1,23 @@
+import random
+
+def qsort(a):
+    less = []
+    equal = []
+    greater = []
+    if len(a) > 1:
+        pivot = a[random.randrange(0, len(a))]
+        for x in a:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            else:
+                greater.append(x)
+        print(less, equal, greater)
+        return qsort(less) + equal + qsort(greater)
+    else:
+        return a
+
+a = [24, 2, 36, 77, 9, 0, 63, 5]
+b = qsort(a)
+print(b)
---
+++ 4-1/quicksort.py
@@ -0,0 +1,60 @@
+import random
+
+def partition(a, left, right):
+    p = random.randint(left, right)
+    if p != left:
+        a[left], a[p] = a[p], a[left]
+    i = left + 1
+    j = right
+    done = False
+    while not done:
+        while i <= j and a[i] <= a[left]: i += 1
+        while i <= j and a[j] >= a[left]: j -= 1
+
+        if i > j:
+            done = True
+        else:
+            a[i], a[j] = a[j], a[i]
+
+    a[left], a[j] = a[j], a[left]
+    return j
+
+
+def quicksort(a, left, right):
+    if left < right:
+        p = partition(a, left, right)
+        quicksort(a, left, p - 1)
+        quicksort(a, p + 1, right)
+
+a = [24, 2, 36, 77, 9, 0, 63, 5]
+quicksort(a, 0, len(a)-1)
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

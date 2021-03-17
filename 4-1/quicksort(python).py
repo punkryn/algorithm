2e5245306608d260b4acb958c333b3710a17ce8a
punkryn <gns8167@naver.com>
2021-03-17 10:27:15 +0900

    2021-03-17 qsort

--- 4-1/quicksort(python).py
+++ 4-1/quicksort(python).py
@@ -1,19 +1,46 @@
+# import random
+#
+# def qsort(a):
+#     less = []
+#     equal = []
+#     greater = []
+#     if len(a) > 1:
+#         pivot = a[random.randrange(0, len(a))]
+#         for x in a:
+#             if x < pivot:
+#                 less.append(x)
+#             elif x == pivot:
+#                 equal.append(x)
+#             else:
+#                 greater.append(x)
+#         print(less, equal, greater)
+#         return qsort(less) + equal + qsort(greater)
+#     else:
+#         return a
+#
+# a = [24, 2, 36, 77, 9, 0, 63, 5]
+# b = qsort(a)
+# print(b)
+
+
 import random
 
 def qsort(a):
     less = []
     equal = []
     greater = []
+
     if len(a) > 1:
-        pivot = a[random.randrange(0, len(a))]
+        pivot = random.randrange(0, len(a))
+
         for x in a:
-            if x < pivot:
+            if x < a[pivot]:
                 less.append(x)
-            elif x == pivot:
+            elif x == a[pivot]:
                 equal.append(x)
             else:
                 greater.append(x)
-        print(less, equal, greater)
+
         return qsort(less) + equal + qsort(greater)
     else:
         return a
@@ -21,3 +48,32 @@ def qsort(a):
 a = [24, 2, 36, 77, 9, 0, 63, 5]
 b = qsort(a)
 print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

4ef6f81312a66c066b6df4e0a5931b4d49cf8fb1
punkryn <gns8167@naver.com>
2021-03-22 11:11:03 +0900

    2021-03-22 mergesort(list)

--- 4-1/mergesort(list).py
+++ 4-1/mergesort(list).py
@@ -1,29 +1,59 @@
-def mergesort(arr):
-    if len(arr) < 2:
-        return arr
+# def mergesort(arr):
+#     if len(arr) < 2:
+#         return arr
+#
+#     mid = len(arr) // 2
+#     low_arr = mergesort(arr[:mid])
+#     high_arr = mergesort(arr[mid:])
+#     #print(low_arr, high_arr)
+#
+#     merged_arr = []
+#     l = h = 0
+#     while l < len(low_arr) and h < len(high_arr):
+#         if low_arr[l] < high_arr[h]:
+#             merged_arr.append(low_arr[l])
+#             l += 1
+#         else:
+#             merged_arr.append(high_arr[h])
+#             h += 1
+#     merged_arr += low_arr[l:]
+#     merged_arr += high_arr[h:]
+#
+#     return merged_arr
+#
+#
+# alist = [4, 26, 9, 3, 1, 72, 566, 43]
+# blist = mergesort(alist)
+# print(blist)
+#
 
-    mid = len(arr) // 2
-    low_arr = mergesort(arr[:mid])
-    high_arr = mergesort(arr[mid:])
-    #print(low_arr, high_arr)
 
-    merged_arr = []
-    l = h = 0
-    while l < len(low_arr) and h < len(high_arr):
-        if low_arr[l] < high_arr[h]:
-            merged_arr.append(low_arr[l])
-            l += 1
+def mergesort(a):
+    if len(a) > 1:
+        k = (len(a) - 1) // 2
+        return merge(mergesort(a[:k+1]), mergesort(a[k+1:]))
+    else:
+        return a
+
+def merge(a, b):
+    c = []
+
+    while a and b:
+        aele = a.pop(0)
+        bele = b.pop(0)
+
+        if aele > bele:
+            c.append(bele)
+            a.insert(0, aele)
+        elif aele < bele:
+            c.append(aele)
+            b.insert(0, bele)
         else:
-            merged_arr.append(high_arr[h])
-            h += 1
-    merged_arr += low_arr[l:]
-    merged_arr += high_arr[h:]
+            c.append(aele)
+            c.append(bele)
 
-    return merged_arr
-
+    return c + a + b
 
 alist = [4, 26, 9, 3, 1, 72, 566, 43]
 blist = mergesort(alist)
 print(blist)
-
-

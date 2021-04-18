191ee64b7a821567df37a1362152ecf757048261
punkryn <gns8167@naver.com>
2021-04-18 16:19:06 +0900

    2021-04-18 cant finish player

---
+++ programmers/cantFinishPlayer.py
@@ -0,0 +1,26 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42576?language=python3
+
+def solution(participant, completion):
+    answer = ''
+    players = dict()
+    for player in participant:
+        if players.get(player):
+            players[player] += 1
+        else:
+            players[player] = 1
+
+    for player in completion:
+        if player in players.keys():
+            players[player] -= 1
+
+    for key in players.keys():
+        if players[key] == 1:
+            answer = key
+
+    return answer
+
+
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+
+print(solution(participant, completion))
